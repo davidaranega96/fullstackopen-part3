148e359f9e40986e299c5701d003dca79eaac001
daranega <daranega@optimusprice.ai>
2023-11-23 17:23:13 +0100

    refactored the folders

---
+++ phonebook-frontend/src/services/persons.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const baseUrl = '/api/persons'
+
+const getAll = () => {
+  console.log(baseUrl)
+  const request = axios.get(baseUrl)
+  console.log(request)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const delete_entry = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    console.log(request.data)
+    return request.then(response => getAll())
+}
+
+export default { getAll, create, update, delete_entry }
